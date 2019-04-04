f77b33841897bce3732822546df25a8309978be0
KP McDonnell <kmcdonnell.it@gmail.com>
2019-04-04 12:27:06 +0100

    changed landing title

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -68,7 +68,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1>Welcome to KPMD.DEV</h1>
 
 	<div id="body">
 		<p>Under Construction.</p>
