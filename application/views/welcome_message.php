ad5d6adfb8144e8a5206eda934bf002306f2330e
KP McDonnell <kmcdonnell.it@gmail.com>
2019-04-04 12:28:37 +0100

    removed render time footer

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -69,12 +69,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <div id="container">
 	<h1>Welcome to KPMD.DEV</h1>
-
 	<div id="body">
 		<p>Under Construction.</p>
 	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
 
 </body>
